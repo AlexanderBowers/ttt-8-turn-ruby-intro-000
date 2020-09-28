8b7f06b7e105a8bdb0cc5702a71715f335807bc2
Alexander Bowers <7319abowers@gmail.com>
2020-09-28 02:37:07 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,17 @@
+<<<<<<< HEAD
 require "pry"
 
 def greeting(input)
 puts "Please enter 1-9:"
 return input
+=======
+def display_board (board)
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts " -----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts " -----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+>>>>>>> 713aff909544e73e4bc9156258e27b4644e81a4f
 end
 
 def input_to_index(input)
@@ -11,6 +20,7 @@ def input_to_index(input)
   return index
 end
 
+<<<<<<< HEAD
 def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -61,4 +71,31 @@ def turn(board)
   #binding.pry
   move(board, index, character = "X")
   display_board(board)
+=======
+def position_taken?(board, index)
+  taken = board[index] == "X" || "O"
+return taken
+end
+
+def valid_range?(board, index)
+  good_range = board[index] >= 0 && board[index] <= 8
+return good_range
+end
+
+def valid_move(board, index)
+if board[index] = !position_taken && valid_range?(board, index)
+  return valid
+else
+  return !valid
+end
+end
+
+def move(board, index, character = "X")
+  if valid_move(board, index)
+    board[index] = character
+  end
+end
+
+def turn(board, index)
+>>>>>>> 713aff909544e73e4bc9156258e27b4644e81a4f
 end
